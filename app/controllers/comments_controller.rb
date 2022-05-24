4499da5131aa4fc7b88bd3aa042871faf9efb78b
Tarannum <tanu270011@gmail.com>
2022-05-24 17:26:25 +0530

    blog

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = current_user.comments.new(comment_params)
+    if !@comment.save
+      flash[:notice] = @comment.errors.full_messages.to_sentence
+    end
+    redirect_to post_path(params[:post_id])
+  end
+  
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(post_id: params[:post_id])
+  end
+end
